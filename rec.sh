3dd7e3a635e5e7c4bf06ee6502cd1b4fe58c30c0
eisuke42 <134784582+eisuke42@users.noreply.github.com>
2023-05-27 15:53:20 +0200

    Update rec.sh

--- rec.sh
+++ rec.sh
@@ -3,14 +3,17 @@ g++ tests.cpp -o tests
 
 g++ recuit.cpp -o recuit
 ./recuit < test1.in > recuit.out
+echo -n "recuit simulé : "
 tail -n 1 recuit.out
 
 g++ swaps.cpp -o swaps 
 ./swaps < test1.in > swaps.out
+echo -n "swaps : "
 tail -n 1 swaps.out
 
 g++ desc.cpp -o desc
 ./desc < test1.in > desc.out
+echo -n "descente locale : "
 tail -n 1 desc.out
 
 python3 graphique.py
